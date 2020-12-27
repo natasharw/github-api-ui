486b9a5e91770e52bcef319bfc713fbbaeb768c7
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-27 19:06:54 +0000

    add search screen UI with programmatic constraints

---
+++ GithubAPIViewer/GAVButton.swift
@@ -0,0 +1,38 @@
+//
+//  GAVButton.swift
+//  GithubAPIViewer
+//
+//  Created by Natasha Wilson on 27/12/2020.
+//
+
+import UIKit
+
+class GAVButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
