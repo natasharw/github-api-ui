3ae27d8b1d78dd06a212da739e3e760235ec1b2f
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-27 13:27:36 +0000

    Add inital files and search and favourties VCs

---
+++ GHFollowers/FavouriteListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteListVC.swift
+//  GHFollowers
+//
+//  Created by Natasha Wilson on 27/12/2020.
+//
+
+import UIKit
+
+class FavouriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Natasha Wilson on 27/12/2020.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
