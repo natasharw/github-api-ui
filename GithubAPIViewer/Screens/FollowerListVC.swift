548aa43a3e546b623a389d151797cb0b6796b1c1
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-28 17:28:21 +0000

    get flow layout collection view working using diffable data source

--- GithubAPIViewer/Screens/FollowerListVC.swift
+++ GithubAPIViewer/Screens/FollowerListVC.swift
@@ -12,8 +12,10 @@ class FollowerListVC: UIViewController {
     enum Section {
         case main
     }
-    
+
     var username: String!
+    var followers: [Follower] = []
+
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
@@ -43,7 +45,7 @@ class FollowerListVC: UIViewController {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
@@ -68,8 +70,9 @@ class FollowerListVC: UIViewController {
             
             switch result {
             case .success(let followers):
-                print("Followers.count = \(followers.count)")
-                print(followers)
+                self.followers = followers
+                self.updateData()
+
             case .failure(let error):
                 self.presentGAVAlertOnMainThread(alertTitle: "Bad stuff happened", message: error.rawValue, buttonTitle: "Ok")
             }
@@ -83,5 +86,13 @@ class FollowerListVC: UIViewController {
             cell.set(follower: follower)
             return cell
         })
+    }
+
+
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
     }
 }
