94efebf4b3829308ae835d346f62dc39f7e9e9be
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-28 21:41:13 +0000

    add loading screen while network calls are happening

--- GithubAPIViewer/Extensions/UIViewController+Ext.swift
+++ GithubAPIViewer/Extensions/UIViewController+Ext.swift
@@ -7,14 +7,48 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
-    
+
     func presentGAVAlertOnMainThread(alertTitle: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
             let alertVC = GAVAlertVC(alertTitle: alertTitle, message: message, buttonTitle: buttonTitle)
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
+        }
+    }
+
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+
+        containerView.backgroundColor   = .systemBackground
+        containerView.alpha             = 0
+
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+
+        ])
+
+        activityIndicator.startAnimating()
+    }
+    
+
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
