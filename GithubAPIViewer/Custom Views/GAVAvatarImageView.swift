27b4f04714df775c1650596d421a0b624ea168be
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-28 16:34:13 +0000

    add custom follower cell

---
+++ GithubAPIViewer/Custom Views/GAVAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GAVAvatarImageView.swift
+//  GithubAPIViewer
+//
+//  Created by Natasha Wilson on 28/12/2020.
+//
+
+import UIKit
+
+class GAVAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius  = 10
+        clipsToBounds       = true
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
