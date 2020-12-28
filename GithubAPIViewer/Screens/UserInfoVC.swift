911ea11ea8b98752723d6b606bef4849483a6229
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-28 23:07:34 +0000

    add done button on right hand side of nav bar on user modal

--- GithubAPIViewer/Screens/UserInfoVC.swift
+++ GithubAPIViewer/Screens/UserInfoVC.swift
@@ -11,9 +11,15 @@ class UserInfoVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
     }
-    
+
+
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
 
 
 
