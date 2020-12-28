861aad92f91c24676c2ad6d6f931e84209627f5a
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-28 17:54:09 +0000

    download avatar images and display in the follower cell

--- GithubAPIViewer/Custom Views/GAVAvatarImageView.swift
+++ GithubAPIViewer/Custom Views/GAVAvatarImageView.swift
@@ -15,18 +15,37 @@ class GAVAvatarImageView: UIImageView {
         super.init(frame: frame)
         configure()
     }
-    
-    
+
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
+
+
     private func configure() {
         layer.cornerRadius  = 10
         clipsToBounds       = true
         image               = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
     }
-    
+
+
+    func downloadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+
+            guard let image = UIImage(data: data) else { return }
+
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+
+        task.resume()
+    }
 }
