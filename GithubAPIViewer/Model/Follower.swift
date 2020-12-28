cbd80424617e7c3628004b29acb47029fdd4644f
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-28 14:40:37 +0000

    add user and follower models

---
+++ GithubAPIViewer/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubAPIViewer
+//
+//  Created by Natasha Wilson on 28/12/2020.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
