6426d92c321eb3aa987dd3df70d49a9a4c3c1f88
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-29 21:12:58 +0000

    add swipe to delete functionality on favourites table

--- GithubAPIViewer/Screens/FavouriteListVC.swift
+++ GithubAPIViewer/Screens/FavouriteListVC.swift
@@ -52,15 +52,11 @@ class FavouriteListVC: UIViewController {
                     self.showEmptyStateView(with: "No users have been added to favourites yet", in: self.view)
                 } else {
                     self.favourites = favourites
-                    print(self.favourites)
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
                         self.view.bringSubviewToFront(self.tableView)
                     }
                 }
-                
-                print("I am here")
-
                 
             case .failure(let error):
                 self.presentGAVAlertOnMainThread(alertTitle: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
@@ -80,7 +76,6 @@ extension FavouriteListVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: FavouriteCell.reuseID) as! FavouriteCell
         let favourite = favourites[indexPath.row]
-        print(favourite)
         cell.set(favourite: favourite)
         return cell
     }
@@ -93,5 +88,23 @@ extension FavouriteListVC: UITableViewDataSource, UITableViewDelegate {
         destVC.title        = favourite.login
         
         navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favourite       = favourites[indexPath.row]
+        favourites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        
+        PersistenceManager.updateWith(favourite: favourite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            
+            guard let error = error else { return }
+            
+            self.presentGAVAlertOnMainThread(alertTitle: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
     }
 }
