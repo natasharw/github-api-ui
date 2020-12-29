fa3ab1529021b36715dd7206472c371568c690dd
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-29 22:42:20 +0000

    change decoding strategy to go directly to date type for createdAt in user model

--- GithubAPIViewer/Extensions/Date+Ext.swift
+++ GithubAPIViewer/Extensions/Date+Ext.swift
@@ -10,8 +10,8 @@ import Foundation
 extension Date {
     
     func convertToMonthYearFormat() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM yyyy"
+        let dateFormatter           = DateFormatter()
+        dateFormatter.dateFormat    = "MMM yyyy"
         
         return dateFormatter.string(from: self)
     }
--- GithubAPIViewer/Screens/UserInfoVC.swift
+++ GithubAPIViewer/Screens/UserInfoVC.swift
@@ -65,7 +65,7 @@ class UserInfoVC: UIViewController {
         self.add(childVC: GAVUserInfoHeaderVC(user: user), to: self.headerView)
         self.add(childVC: repoItemVC, to: self.itemViewOne)
         self.add(childVC: followerItemVC, to: self.itemViewTwo)
-        self.dateLabel.text = "GitHub user since \(user.createdAt.convertToDisplayFormat())"
+        self.dateLabel.text = "GitHub user since \(user.createdAt.convertToMonthYearFormat())"
     }
 
 
