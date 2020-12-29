8a4a0108793cb26d6d268f5952be5506d9f88662
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-29 18:28:52 +0000

    fix delegates by making them a weak var to avoid retain cycle and memory leaks

--- GithubAPIViewer/CustomViews/ViewControllers/ItemInfoVCs/GAVItemInfoVC.swift
+++ GithubAPIViewer/CustomViews/ViewControllers/ItemInfoVCs/GAVItemInfoVC.swift
@@ -15,7 +15,7 @@ class GAVItemInfoVC: UIViewController {
     let actionButton    = GAVButton()
     
     var user: User!
-    var delegate: UserInfoVCDelegate!
+    weak var delegate: UserInfoVCDelegate!
     
     init(user: User) {
         super.init(nibName: nil, bundle: nil)
