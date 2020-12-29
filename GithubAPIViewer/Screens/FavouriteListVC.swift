6a806efcef70c8e9161f97cf34e91608dd4606f5
Natasha Wilson <wilson.natasharose@gmail.com>
2020-12-29 20:14:57 +0000

    Use persistence manager to retrive favourites in favourite screen and print to console

--- GithubAPIViewer/Screens/FavouriteListVC.swift
+++ GithubAPIViewer/Screens/FavouriteListVC.swift
@@ -12,6 +12,15 @@ class FavouriteListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavourites { result in
+            switch result {
+            case .success(let favourites):
+                print(favourites)
+            case .failure(let error):
+                break
+            }
+        }
     }
     
 
